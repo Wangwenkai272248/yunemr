d91be1beb7f2c79a4679c7799b819b860a71dd4c
zhouziyu <1107536493@qq.com>
2018-09-11 15:27:41 +0800

    notuse

--- clinic-cms/src/main/java/jhmk/clinic/cms/controller/testController/MainDisController.java
+++ clinic-cms/src/main/java/jhmk/clinic/cms/controller/testController/MainDisController.java
@@ -1,9 +1,16 @@
 package jhmk.clinic.cms.controller.testController;
 
+import jhmk.clinic.cms.controller.ruleService.SyzdService;
+import jhmk.clinic.cms.service.Write2File;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author ziyu.zhou
@@ -12,8 +19,17 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 @RequestMapping("/test/dis")
 public class MainDisController {
-    //获取出院主诊断+个数
-    public void getAllData(HttpServletResponse response){
+    @Autowired
+    SyzdService syzdService;
 
+    //获取出院主诊断+个数
+    @PostMapping("/getAllData")
+    public void getAllData(HttpServletResponse response) {
+        Map<String, Integer> allData = syzdService.getAllData();
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : allData.entrySet()) {
+            list.add(entry.getKey() + "/" + entry.getValue());
+        }
+        Write2File.w2fileList(list,"/data/1/CDSS/allData.txt");
     }
 }
