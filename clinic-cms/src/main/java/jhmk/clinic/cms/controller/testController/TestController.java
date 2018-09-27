e3736a455cd76bf1ad741fee987f891ce6ec4125
zhouziyu <1107536493@qq.com>
2018-09-27 15:53:39 +0800

    统计疾病数量

---
+++ clinic-cms/src/main/java/jhmk/clinic/cms/controller/testController/TestController.java
@@ -0,0 +1,53 @@
+package jhmk.clinic.cms.controller.testController;
+
+import com.alibaba.fastjson.JSONObject;
+import jhmk.clinic.cms.controller.ruleService.BasyService;
+import jhmk.clinic.cms.controller.ruleService.SyzdService;
+import jhmk.clinic.cms.function.demo1.Demo1Service;
+import jhmk.clinic.cms.service.Write2File;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author ziyu.zhou
+ * @date 2018/9/27 14:51
+ */
+@Controller
+@RequestMapping("/test/tests")
+public class TestController {
+    @Autowired
+    Demo1Service demo1Service;
+    @Autowired
+    BasyService basyService;
+    @Autowired
+    SyzdService syzdService;
+
+    @PostMapping("/demo1")
+    public void getAllData(HttpServletResponse response, @RequestBody(required = false) String map) {
+        Map<String, Integer> resuiltMap = new HashMap<>();
+        final Set<String> syzdByDiseaseName = syzdService.getSyzdByDiseaseName(map);
+        final Map<String, Set<String>> stringSetMap = demo1Service.selYizhuById(syzdByDiseaseName);
+        for (Map.Entry<String, Set<String>> entry : stringSetMap.entrySet()) {
+            Set<String> value = entry.getValue();
+            for (String name : value) {
+                if (resuiltMap.containsKey(name)) {
+                    resuiltMap.put(name, resuiltMap.get(name) + 1);
+                } else {
+                    resuiltMap.put(name, 1);
+
+                }
+            }
+        }
+        String fileNmae = "/data/1/CDSS/"+map+".txt";
+        Write2File.wfile(JSONObject.toJSONString(resuiltMap),fileNmae);
+        System.out.println("写完啦");
+    }
+}
