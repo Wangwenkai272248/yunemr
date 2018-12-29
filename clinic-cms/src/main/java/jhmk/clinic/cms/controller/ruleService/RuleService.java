0c4faec013fac567fc4760414f3882b678841355
zhouziyu <1107536493@qq.com>
2018-12-29 17:43:41 +0800

    3院 统计治疗方案

---
+++ clinic-cms/src/main/java/jhmk/clinic/cms/controller/ruleService/RuleService.java
@@ -0,0 +1,11 @@
+package jhmk.clinic.cms.controller.ruleService;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author ziyu.zhou
+ * @date 2018/12/29 15:32
+ */
+@Service
+public class RuleService {
+}
