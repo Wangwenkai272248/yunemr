4882af7369ff6a0957b48def62c70d9581672eaf
zhouziyu <1107536493@qq.com>
2018-12-29 18:09:55 +0800

    3院 统计治疗方案

---
+++ clinic-cms/src/main/java/jhmk/clinic/cms/testcontroller/Config.java
@@ -0,0 +1,13 @@
+package jhmk.clinic.cms.testcontroller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * @author ziyu.zhou
+ * @date 2018/12/27 16:15
+ */
+@Configuration
+@ImportResource(locations = {"classpath:spring-config.xml"})
+public class Config {
+}
