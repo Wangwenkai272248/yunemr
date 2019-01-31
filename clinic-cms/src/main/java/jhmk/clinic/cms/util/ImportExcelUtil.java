4149cfe6909099be50fb4b46e7646b68c1475605
songwq <1260848371@qq.com>
2019-01-31 10:51:36 +0800

    增加excel导入多sheet页解析

--- clinic-cms/src/main/java/jhmk/clinic/cms/util/ImportExcelUtil.java
+++ clinic-cms/src/main/java/jhmk/clinic/cms/util/ImportExcelUtil.java
@@ -24,9 +24,7 @@ public class ImportExcelUtil {
     private static Workbook wb = null;
     private static Sheet sheet = null;
 
-    public static Map<String,Object> parseExcelData(MultipartFile file){
-        Map<String,Object> map = new HashMap<>();
-        List<List<Object>> listObject = new ArrayList<>();
+    public static List<Map<String,Object>> parseExcelData(MultipartFile file){
         String originalFileName = file.getOriginalFilename();
         try{
             if (originalFileName.endsWith(".xlsx")) {
@@ -38,37 +36,48 @@ public class ImportExcelUtil {
             e.printStackTrace();
             return null;
         }
-        sheet = wb.getSheetAt(0);
-        //获得总列数
-        int coloumNum=sheet.getRow(0).getPhysicalNumberOfCells();
-        List headers = new ArrayList(coloumNum);
+        List<Map<String,Object>> listSheets = new ArrayList<>();
+        for(int sheetSize=0;sheetSize<wb.getNumberOfSheets();sheetSize++){
+            Map<String,Object> map = new HashMap<>();
+            List<List<Object>> listObject = new ArrayList<>();
+            sheet = wb.getSheetAt(sheetSize);
+            //获得总列数
+            int coloumNum=sheet.getRow(0).getPhysicalNumberOfCells();
+            List headers = new ArrayList(coloumNum);
 
-        //默认为总行数-1
-        for (int i = 0; i < sheet.getLastRowNum()+1; i++){
-            List<Object> list = new ArrayList<>();
-            //获取索引为i的行，以0开始
-            Row row = sheet.getRow(i);
-            for(int j=0;j<coloumNum;j++){
-                //把第一行的表头存起来
-                if(i==0){
-                    headers.add(row.getCell(j).getStringCellValue());
-                }else{
-                    String name = "";
-                    //先设置单个cell的类型，否则遇到数字类型，getStringCellValue会报异常
-                    if(row !=null && row.getCell(j)!=null){
-                        row.getCell(j).setCellType(Cell.CELL_TYPE_STRING);
-                        //获取第i行的索引为j的单元格数据
-                        name= row.getCell(j).getStringCellValue();
+            //默认为总行数-1
+            for (int i = 0; i < sheet.getLastRowNum()+1; i++){
+                List<Object> list = new ArrayList<>();
+                //获取索引为i的行，以0开始
+                Row row = sheet.getRow(i);
+                for(int j=0;j<coloumNum;j++){
+                    //把第一行的表头存起来
+                    if(i==0){
+                        if(row !=null && row.getCell(j)!=null) {
+                            headers.add(row.getCell(j).getStringCellValue());
+                        }else{
+                            headers.add("");
+                        }
+                    }else{
+                        String name = "";
+                        //先设置单个cell的类型，否则遇到数字类型，getStringCellValue会报异常
+                        if(row !=null && row.getCell(j)!=null){
+                            row.getCell(j).setCellType(Cell.CELL_TYPE_STRING);
+                            //获取第i行的索引为j的单元格数据
+                            name= row.getCell(j).getStringCellValue();
+                        }
+                        list.add(name);
                     }
-                    list.add(name);
+                }
+                if(i!=0){
+                    listObject.add(list);
                 }
             }
-            if(i!=0){
-                listObject.add(list);
-            }
+            map.put("headers",headers);
+            map.put("data",listObject);
+            listSheets.add(map);
         }
-        map.put("headers",headers);
-        map.put("data",listObject);
-        return map;
+
+        return listSheets;
     }
 }
